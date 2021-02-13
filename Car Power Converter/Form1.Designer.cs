f6c5bf65707a390e9f49270694181e2533f36e67
Heli-o <vasikmares@gmail.com>
2021-02-13 13:46:31 +0100

    main update
    Added the VS project files

---
+++ Car Power Converter/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿
+namespace Car_Power_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.kilowatts = new System.Windows.Forms.TextBox();
+            this.horsepower = new System.Windows.Forms.TextBox();
+            this.rpm = new System.Windows.Forms.TextBox();
+            this.torque = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.torqueSeperate = new System.Windows.Forms.TextBox();
+            this.speedSeperate = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.kilowattsSeperate = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.update = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kilowatts";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Horsepower";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 81);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hp";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 54);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "kW";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 115);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 18);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Torque";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(55, 144);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 18);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Speed";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(275, 112);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Nm";
+            // 
+            // kilowatts
+            // 
+            this.kilowatts.Location = new System.Drawing.Point(128, 51);
+            this.kilowatts.Name = "kilowatts";
+            this.kilowatts.Size = new System.Drawing.Size(139, 24);
+            this.kilowatts.TabIndex = 13;
+            this.kilowatts.TextChanged += new System.EventHandler(this.updateCalculations);
+            // 
+            // horsepower
+            // 
+            this.horsepower.Location = new System.Drawing.Point(128, 81);
+            this.horsepower.Name = "horsepower";
+            this.horsepower.Size = new System.Drawing.Size(139, 24);
+            this.horsepower.TabIndex = 14;
+            // 
+            // rpm
+            // 
+            this.rpm.Location = new System.Drawing.Point(128, 141);
+            this.rpm.Name = "rpm";
+            this.rpm.Size = new System.Drawing.Size(139, 24);
+            this.rpm.TabIndex = 15;
+            this.rpm.TextChanged += new System.EventHandler(this.updateCalculations);
+            // 
+            // torque
+            // 
+            this.torque.Location = new System.Drawing.Point(128, 111);
+            this.torque.Name = "torque";
+            this.torque.Size = new System.Drawing.Size(139, 24);
+            this.torque.TabIndex = 16;
+            this.torque.TextChanged += new System.EventHandler(this.updateCalculations);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(273, 141);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 18);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "RPM";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(381, 17);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(257, 18);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Calculate kWs from torque and speed";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(125, 17);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(128, 18);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Main Conversions";
+            // 
+            // torqueSeperate
+            // 
+            this.torqueSeperate.Location = new System.Drawing.Point(460, 50);
+            this.torqueSeperate.Name = "torqueSeperate";
+            this.torqueSeperate.Size = new System.Drawing.Size(139, 24);
+            this.torqueSeperate.TabIndex = 20;
+            this.torqueSeperate.TextChanged += new System.EventHandler(this.torSpeedKilos);
+            // 
+            // speedSeperate
+            // 
+            this.speedSeperate.Location = new System.Drawing.Point(460, 78);
+            this.speedSeperate.Name = "speedSeperate";
+            this.speedSeperate.Size = new System.Drawing.Size(139, 24);
+            this.speedSeperate.TabIndex = 21;
+            this.speedSeperate.TextChanged += new System.EventHandler(this.torSpeedKilos);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(387, 82);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(50, 18);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Speed";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(606, 81);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(42, 18);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "RPM";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(381, 53);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(55, 18);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "Torque";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(606, 55);
+            this.label14.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(32, 18);
+            this.label14.TabIndex = 25;
+            this.label14.Text = "Nm";
+            // 
+            // kilowattsSeperate
+            // 
+            this.kilowattsSeperate.Location = new System.Drawing.Point(460, 109);
+            this.kilowattsSeperate.Name = "kilowattsSeperate";
+            this.kilowattsSeperate.Size = new System.Drawing.Size(139, 24);
+            this.kilowattsSeperate.TabIndex = 26;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(371, 112);
+            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(68, 18);
+            this.label15.TabIndex = 27;
+            this.label15.Text = "Kilowatts";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(606, 112);
+            this.label16.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(31, 18);
+            this.label16.TabIndex = 28;
+            this.label16.Text = "kW";
+            // 
+            // update
+            // 
+            this.update.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.update.Location = new System.Drawing.Point(408, 139);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(240, 26);
+            this.update.TabIndex = 29;
+            this.update.Text = "Click here to update the main conversion";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.update_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(691, 180);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.kilowattsSeperate);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.speedSeperate);
+            this.Controls.Add(this.torqueSeperate);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.torque);
+            this.Controls.Add(this.rpm);
+            this.Controls.Add(this.horsepower);
+            this.Controls.Add(this.kilowatts);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Car Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox kilowatts;
+        private System.Windows.Forms.TextBox horsepower;
+        private System.Windows.Forms.TextBox rpm;
+        private System.Windows.Forms.TextBox torque;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox torqueSeperate;
+        private System.Windows.Forms.TextBox speedSeperate;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox kilowattsSeperate;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Button update;
+    }
+}
+
---
+++ Car Power Converter/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_Power_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void updateCalculations(object sender, EventArgs e)
+        {
+            bool status = false;
+            bool status2 = false;
+            double kilos;
+            switch ((sender as Control).Name)
+            {
+                case "kilowatts":
+                    status = double.TryParse(kilowatts.Text, out kilos);
+                    if (!status) return;
+                    horsepower.Text = Convert.ToString(kilos / 0.745699872);
+                    break;
+                case "horsepower":
+                    status = double.TryParse(horsepower.Text, out double hps);
+                    if (!status) return;
+                    kilowatts.Text = Convert.ToString(0.745699872 * hps);
+                    break;
+                case "rpm":
+                    status = double.TryParse(kilowatts.Text, out kilos);
+                    status2 = double.TryParse(rpm.Text, out double speed);
+                    if (!status || !status2) return;
+                    torque.Text = Convert.ToString(9.5488 * kilos / speed);
+                    break;
+                case "torque":
+                    status = double.TryParse(kilowatts.Text, out kilos);
+                    status2 = double.TryParse(torque.Text, out double torq);
+                    if (!status || !status2) return;
+                    rpm.Text = Convert.ToString(9.5488 * kilos / torq);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void torSpeedKilos(object sender, EventArgs e)
+        {
+            bool status = double.TryParse(torqueSeperate.Text, out double torq);
+            bool status2 = double.TryParse(speedSeperate.Text, out double speed);
+            if (!status || !status2) return;
+            kilowattsSeperate.Text = Convert.ToString(torq * speed / 9.5488);
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+            kilowatts.Text = kilowattsSeperate.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(707, 219);
+        }
+    }
+}
